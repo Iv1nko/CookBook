d9b9853419eff9f269e78fc08a4851074e8858d6
AizeRs <NAKorshunov@mail.ru>
2022-04-27 20:15:51 +0500

    add simple list

---
+++ app/src/main/java/com/example/cookbook/MainFragment.kt
@@ -0,0 +1,27 @@
+package com.example.cookbook
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+
+class MainFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val data = listOf<String>("dish 1", "dish 2", "dish 3")
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+        val list = view.findViewById<RecyclerView>(R.id.list)
+        val adapter = MainListAdapter()
+        adapter.data = data
+        list.adapter = adapter
+        return view
+    }
+}
---
+++ app/src/main/java/com/example/cookbook/MainListAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.cookbook
+
+import android.view.LayoutInflater
+import android.view.TextureView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MainListAdapter : RecyclerView.Adapter<MainListAdapter.MainListViewHolder>() {
+
+
+    var data = listOf<String>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainListViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return MainListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MainListViewHolder, position: Int) {
+        val dish = data.get(position)
+        holder.rootView.findViewById<TextView>(R.id.title).text = dish
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+
+    class MainListViewHolder(val rootView: View) : RecyclerView.ViewHolder(rootView) {
+
+    }
+}
